dbe3c41727b84c96e5a3dea218665c72669c65cd
Andrew F. Davis <afd@ti.com>
2018-05-04 19:06:09 +0000

    ti: k3: drivers: Add support for TI System Control Interface protocol
    Texas Instrument's System Control Interface (TI-SCI) Message Protocol
    is used in Texas Instrument's System on Chip (SoC) such as those
    in K3 family AM654x SoCs to communicate between various compute
    processors with a central system controller entity.
    
    TI-SCI message protocol provides support for management of various
    hardware entities within the SoC. Add support driver to allow
    communication with system controller entity within the SoC.
    
    This is mostly derived from TI-SCI driver in Linux and U-Boot.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Reviewed-by: Andreas Dannenberg <dannenberg@ti.com>

---
+++ plat/ti/k3/common/drivers/ti_sci/ti_sci.h
@@ -0,0 +1,20 @@
+/*
+ * Texas Instruments System Control Interface API
+ *   Based on Linux and U-Boot implementation
+ *
+ * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __TISCI_PROTOCOL_H
+#define __TISCI_PROTOCOL_H
+
+/**
+ * ti_sci_init() - Basic initialization
+ *
+ * Return: 0 if all goes good, else appropriate error message.
+ */
+int ti_sci_init(void);
+
+#endif	/* __TISCI_PROTOCOL_H */
