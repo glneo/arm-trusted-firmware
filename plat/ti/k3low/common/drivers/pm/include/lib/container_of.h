bd0e225538f659b2845ec4d05649565d27f831de
Andrew Davis <afd@ti.com>
2025-10-20 12:54:20 -0500

    siplify container_of
    Change-Id: I3308faf2aec2215fc316ed6054b2b5e90cc8a13b

--- plat/ti/k3low/common/drivers/pm/include/lib/container_of.h
+++ plat/ti/k3low/common/drivers/pm/include/lib/container_of.h
@@ -1,9 +1,12 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
-#ifndef CCAN_CONTAINER_OF_H
-#define CCAN_CONTAINER_OF_H
-#include <stddef.h>
+#ifndef CONTAINER_OF_H
+#define CONTAINER_OF_H
 
-#include <check_type.h>
+#include <stddef.h>
+#include <build_assert.h>
+
+#define check_types_match(expr1, expr2) \
+	BUILD_ASSERT_OR_ZERO(sizeof(expr1) == sizeof(expr2))
 
 /**
  * container_of - get pointer to enclosing structure
@@ -29,115 +32,8 @@
  *		return container_of(foo, struct info, my_foo);
  *	}
  */
-#define container_of(member_ptr, containing_type, member)		\
-	 ((containing_type *)						\
-	  ((char *)(member_ptr)						\
-	   - container_off(containing_type, member))			\
+#define container_of(member_ptr, containing_type, member) \
+	 ((containing_type *) ((char *)(member_ptr) - offsetof(containing_type, member)) \
 	  + check_types_match(*(member_ptr), ((containing_type *)0)->member))
 
-/**
- * container_of_or_null - get pointer to enclosing structure, or NULL
- * @member_ptr: pointer to the structure member
- * @containing_type: the type this member is within
- * @member: the name of this member within the structure.
- *
- * Given a pointer to a member of a structure, this macro does pointer
- * subtraction to return the pointer to the enclosing type, unless it
- * is given NULL, in which case it also returns NULL.
- *
- * Example:
- *	struct foo {
- *		int fielda, fieldb;
- *		// ...
- *	};
- *	struct info {
- *		int some_other_field;
- *		struct foo my_foo;
- *	};
- *
- *	static struct info *foo_to_info_allowing_null(struct foo *foo)
- *	{
- *		return container_of_or_null(foo, struct info, my_foo);
- *	}
- */
-static inline char *container_of_or_null_(void *member_ptr, size_t offset)
-{
-	return member_ptr ? (char *)member_ptr - offset : NULL;
-}
-#define container_of_or_null(member_ptr, containing_type, member)	\
-	((containing_type *)						\
-	 container_of_or_null_(member_ptr,				\
-			       container_off(containing_type, member))	\
-	 + check_types_match(*(member_ptr), ((containing_type *)0)->member))
-
-/**
- * container_off - get offset to enclosing structure
- * @containing_type: the type this member is within
- * @member: the name of this member within the structure.
- *
- * Given a pointer to a member of a structure, this macro does
- * typechecking and figures out the offset to the enclosing type.
- *
- * Example:
- *	struct foo {
- *		int fielda, fieldb;
- *		// ...
- *	};
- *	struct info {
- *		int some_other_field;
- *		struct foo my_foo;
- *	};
- *
- *	static struct info *foo_to_info(struct foo *foo)
- *	{
- *		size_t off = container_off(struct info, my_foo);
- *		return (void *)((char *)foo - off);
- *	}
- */
-#define container_off(containing_type, member)	\
-	offsetof(containing_type, member)
-
-/**
- * container_of_var - get pointer to enclosing structure using a variable
- * @member_ptr: pointer to the structure member
- * @container_var: a pointer of same type as this member's container
- * @member: the name of this member within the structure.
- *
- * Given a pointer to a member of a structure, this macro does pointer
- * subtraction to return the pointer to the enclosing type.
- *
- * Example:
- *	static struct info *foo_to_i(struct foo *foo)
- *	{
- *		struct info *i = container_of_var(foo, i, my_foo);
- *		return i;
- *	}
- */
-#if HAVE_TYPEOF
-#define container_of_var(member_ptr, container_var, member) \
-	container_of(member_ptr, typeof(*container_var), member)
-#else
-#define container_of_var(member_ptr, container_var, member)	\
-	((void *)((char *)(member_ptr)	-			\
-		  container_off_var(container_var, member)))
-#endif
-
-/**
- * container_off_var - get offset of a field in enclosing structure
- * @container_var: a pointer to a container structure
- * @member: the name of a member within the structure.
- *
- * Given (any) pointer to a structure and a its member name, this
- * macro does pointer subtraction to return offset of member in a
- * structure memory layout.
- *
- */
-#if HAVE_TYPEOF
-#define container_off_var(var, member)		\
-	container_off(typeof(*var), member)
-#else
-#define container_off_var(var, member)			\
-	((const char *)&(var)->member - (const char *)(var))
-#endif
-
-#endif /* CCAN_CONTAINER_OF_H */
+#endif /* CONTAINER_OF_H */
